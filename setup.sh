b51fb1e47f0967be9cac262d7dc1d077644c8d9d
Nathan Novick <nbn22385@gmail.com>
2024-01-22 16:56:53 -0500

    add pd_zurg folders to setup, remove unused service folders

--- setup.sh
+++ setup.sh
@@ -6,10 +6,12 @@ CONFIG_DIR=/config
 DATA_DIR=/mnt/usb
 
 # Make directories
-sudo mkdir -pv ${CONFIG_DIR}/{bazarr,duckdns,grafana,homarr,homeassistant,jellyfin,nginx,overseerr,plex,prowlarr,qbittorrent,radarr,sabnzbd,sonarr,tautulli,wireguard}
+sudo mkdir -pv ${CONFIG_DIR}/{bazarr,duckdns,grafana,jellyfin,nginx,overseerr,plex,prowlarr,qbittorrent,radarr,sabnzbd,sonarr,tautulli,wireguard}
 sudo mkdir -pv ${DATA_DIR}/{torrents,usenet,media}/{tv,movies}
+sudo mkdir -pv /pd_zurg/{cache,config,log,mnt,RD}
 
 # Set permissions
 sudo chmod -Rv 775 ${DATA_DIR}/
 sudo chown -Rv ${USER}:${USER} ${CONFIG_DIR}/
 sudo chown -Rv ${USER}:${USER} ${DATA_DIR}/
+sudo chown -Rv ${USER}:${USER} /pd_zurg
